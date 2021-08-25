507a12f2257b4115538a2fd54732af62296e216f
jasonemarkson <jasonmarkson76@gmail.com>
2021-08-25 15:59:03 -0400

    updated the routes for only specific CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :parks
+  resources :parks, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20210825013020_create_parks.rb
+++ db/migrate/20210825013020_create_parks.rb
@@ -8,6 +8,7 @@ class CreateParks < ActiveRecord::Migration[6.1]
       t.float :longitude
       t.string :images
       t.string :activities
+      # add in state code? create a separate class for State?
 
       t.timestamps
     end
