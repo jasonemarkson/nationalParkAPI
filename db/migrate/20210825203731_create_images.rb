44d94ed7154f4ccaa27bddcbccc2f17cd41fb042
jasonemarkson <jasonmarkson76@gmail.com>
2021-09-01 18:54:15 -0400

    accidently deleted the create images table

--- db/migrate/20210825203731_create_images.rb
+++ db/migrate/20210825203731_create_images.rb
@@ -1,13 +1,13 @@
 class CreateImages < ActiveRecord::Migration[6.1]
-  def change
-    create_table :images do |t|
-      t.string :credit
-      t.string :title
-      t.string :altText
-      t.string :caption
-      t.string :url
-
-      t.timestamps
+    def change
+      create_table :images do |t|
+        t.string :credit
+        t.string :title
+        t.string :altText
+        t.string :caption
+        t.string :url
+  
+        t.timestamps
+      end
     end
   end
-end
